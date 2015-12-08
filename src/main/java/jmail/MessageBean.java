562d20629a18ea1dfaace55a93c28e83175c5353
Ian <ian@darwinsys.com>
2015-12-08 18:51:25 -0500

    Eclipse-generate accessors due to an issue with Lombok.

--- src/main/java/jmail/MessageBean.java
+++ src/main/java/jmail/MessageBean.java
@@ -7,10 +7,35 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@ManagedBean(name="mb") @Getter @Setter @ToString @EqualsAndHashCode
+@ManagedBean(name="mb") @ToString @EqualsAndHashCode
 public class MessageBean {
 	String sender;
 	String recipient;
 	String subject;
 	String body;
+	
+	public String getSender() {
+		return sender;
+	}
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	public String getRecipient() {
+		return recipient;
+	}
+	public void setRecipient(String recipient) {
+		this.recipient = recipient;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
 }
