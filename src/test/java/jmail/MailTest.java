cbb6cbb2ec0cc751f7f27f963f29fdfccaea0878
Ian <ian@darwinsys.com>
2015-12-08 20:01:55 -0500

    Test failure mode too!

--- src/test/java/jmail/MailTest.java
+++ src/test/java/jmail/MailTest.java
@@ -14,7 +14,7 @@ import static org.mockito.Mockito.*;
 public class MailTest {
 	
 	private static final String MESSAGE_ID_HEADER = "message-id";
-	private static final String WANTED_MESSAGE_ID = "JavaMail.Message.12345.GOTYA.COM.erewhon.slugvax";
+	private static final String WANTED_MESSAGE_ID = "<JavaMail.Message.12345.GOTYA.COM.erewhon.slugvax>";
 	private Mail target;
 	
 	@Before
@@ -34,6 +34,19 @@ public class MailTest {
 		Message message = target.findMessageById(WANTED_MESSAGE_ID);
 		assertNotNull(message);
 		assertSame(message, m2);
+	}
+	
+	@Test
+	public void testFindMessageByIdBad() throws MessagingException {
+		Folder f = mock(Folder.class);
+		target.mFolder = f;
+		Message m1 = mock(Message.class);
+		Message m2 = mock(Message.class);
+		when (m1.getHeader(MESSAGE_ID_HEADER)).thenReturn(new String[]{"bleah"});
+		when (m2.getHeader(MESSAGE_ID_HEADER)).thenReturn(new String[]{"meh"});
+		when (f.getMessages()).thenReturn(new Message[]{m1, m2});
+		Message message = target.findMessageById(WANTED_MESSAGE_ID);
+		assertNull(message);
 	}
 
 }
