2b12155a3a3a11aaedf8b548dadbd2fb84dc7abc
Ian <ian@darwinsys.com>
2016-10-13 21:49:39 -0400

    Start more user infra

---
+++ src/main/java/jmail/Settings.java
@@ -0,0 +1,33 @@
+package jmail;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Singleton
+public class Settings {
+
+	@PersistenceContext EntityManager em;
+	
+	List<User> allowedUsers = new ArrayList<>();
+	
+	@PostConstruct
+	public void readUserList() {
+		em.createQuery("from User", User.class).getResultList();
+	}
+
+	public boolean okHtml(User u, String fromAddress) {
+		return false;
+	}
+
+	@TransactionAttribute(TransactionAttributeType.REQUIRED)
+	public void setOkHtml(User u, String fromAddress) {
+		// EMPTY
+	}
+}
---
+++ src/main/java/jmail/User.java
@@ -0,0 +1,56 @@
+package jmail;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+	int id;
+	String firstName, lastName;
+	String login;
+	String passwordHash;
+
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getPasswordHash() {
+		return passwordHash;
+	}
+
+	public void setPasswordHash(String passwordHash) {
+		this.passwordHash = passwordHash;
+	}
+
+}
