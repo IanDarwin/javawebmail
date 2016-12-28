4a6dc986c5a4bbaed744f5657fe867122ff2e361
Ian <ian@darwinsys.com>
2016-12-28 15:35:31 -0500

    Rename db column User->Users as it's a keyword

--- src/main/java/jmail/User.java
+++ src/main/java/jmail/User.java
@@ -4,8 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="Users") // User is a keyword in some databases
 public class User {
 	int id;
 	String firstName, lastName;
